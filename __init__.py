5935701e938429511ec6567d240559850c1c4d3e
Sena <kanada1022@gmail.com>
2024-07-19 18:07:51 +0800

    [ADD] 增加自动悬赏令功能

--- __init__.py
+++ __init__.py
@@ -183,6 +183,7 @@ class XiuXianBot:
             'tpd_timing': Monitor(name='突破丹'),
 
             'zmrw_timing': Monitor(name='宗门任务', time=get_config('Default_ZongMenRenWu', _type='eval')),
+            'xsl_timing': Monitor(name='悬赏令'),
 
             'sc_timing': Monitor(name='收草', time=get_config('Default_ShouCao', _type='eval')),
             'dx_timing': Monitor(name='雕像', time=24 * 60 * 60),
@@ -258,3 +259,6 @@ from . import guanzhu
 
 # 出关
 from . import chuguan
+
+# 悬赏令
+from . import xuanshang
